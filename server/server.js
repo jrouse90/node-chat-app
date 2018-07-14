dfda88b4040bb02453048f5e0c4fcd80954f789e
bobenstien <someguy2361@gmail.com>
2018-07-14 12:46:42 -0400

    Setup the server and front-end

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname, '../public');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(publicPath));
+
+// app.get("/", (req, res) => {
+//     res.render("index.html", {
+//         pageTitle: "Home Page",
+//     });
+// })
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
+
