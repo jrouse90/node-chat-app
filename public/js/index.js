6f34c58a30bf0a1974c93a6bb7dfa969792dd4af
bobenstien <someguy2361@gmail.com>
2018-07-29 14:43:13 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on("connect", () => {
     console.log("Connected to server");
     
-    socket.emit("createMessage", {
-        from: "Frann",
-        text: "tasdt"
-    });
 });
 
 socket.on("newMessage", (message) => {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log("New user connected");
 
-    socket.emit('newMessage',{
-        from:"Bob",
-        text:"Hey what's up?",
-        createdAt:123, 
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit("newMessage", {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
     })
 
     socket.on('disconnect', (socket) => {
